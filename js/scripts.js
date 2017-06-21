da0a59b19ba84f3932d196f42bfe267baa1acd30
Joel Bakken <jkbetc@gmail.com>
2017-06-21 10:54:33 -0700

    add css styles and added a ; to the js code

--- js/scripts.js
+++ js/scripts.js
@@ -1,146 +1,81 @@
 //Business Logic
 
-    function Pizza (size, crust, toppings, sauce) {
-      this.size = size;
-      this.crust = crust;
-      this.toppings = toppings;
-      this.sauce = sauce;
-    }
-
-    Pizza.prototype.price = function() {
-
-      var total1 = 0;
-
-      if (this.size.includes('Large'))
-     {    total1 += 25;
-     }  else if (this.size.includes('Medium'))
-     {    total1 += 19;
-     }  else if (this.size.includes('Small'))
-     {    total1 += 15;
-     }  else if (this.size.includes('Personal'))
-     {    total1 += 10;
-    } else {
-        return NaN;
-      }
-
-    Pizza.prototype.price2 = function() {
-      var total2 = 0;
-
-      if (this.crust.includes('Original'))
-    { total2 += 0;
-    }   else if (this.crust.includes('Thin'))
-    { total2 += 0.5;
-    }   else if (this.crust.includes('Stuffed'))
-    { total2 += 1;
-  } else {
-    return NaN;
-  }
-      consol.log(total2.valueOf)
-}
-this.crust.forEach(function(element){
-  total += itemPrice(element);
-
-  Pizza.prototype.price3 = function() {
-    var total3 = 0;
-
-    if (this.toppings.includes('Pepperoni'))
-   {    total3 += 0.2;
-   }  else if (this.toppings.includes('Breakfast Bacon'))
-   {    total3 += 0.3;
-   }  else if (this.toppings.includes('Sausage'))
-   {    total3 += 0.2;
-   }  else if (this.toppings.includes('Pineapples'))
-   {    total3 += 0.1;
-   }  else if (this.toppings.includes('Black Olives'))
-   {    total3 += 0.1;
-   }  else if (this.toppings.includes('Green Peppers'))
-   {    total3 += 0.1;
-   }  else if (this.toppings.includes('Mushrooms'))
-   {    total3 += 0.1;
-   }  else if (this.toppings.includes('Diced Tomatoes'))
-   {    total3 += 0.1;
-  } else {
-      return NaN;
-    }
-  }
-
-      this.toppings.forEach(function(element){
-        total += itemPrice(element);
-      });
-
-      Pizza.prototype.price4 = function() {
-        var total4 = 0;
-
-        if (this.sauce.includes('tomato'))
-        {   total4 += 0;
-        } else if ( this.sauce.includes('alfredo'))
-        {   total4 += 0.5;
-        }  else if (this.sauce.includes('marinara'))
-        {   total4 += 0.3;
-        }
-
-        var total = (total1 + total2 + total3 + total4);
-
-
-      return total;
-    };
-  });
+function Pizza (size, crust, toppings, sauce) {
+  this.size = size;
+  this.crust = crust;
+  this.toppings = toppings;
+  this.sauce = sauce;
+  this.cost = 0;
 };
 
-  var itemPrice = function(item) {
+Pizza.prototype.priceForSize = function() {
+  if (this.size === 'large')  {
+    this.cost += 25;
+ }  else if (this.size === 'medium') {
+    this.cost += 19;
+ }  else if (this.size === 'small') {
+    this.cost += 15;
+ }  else {
+    this.cost += 10;
+ };
+ return this.cost;
+};
 
-    var items = "";
+Pizza.prototype.priceForCrust = function() {
+    if (this.crust === 'original')  {
+    this.cost += 0;
+  } else if (this.crust === 'thin') {
+    this.cost += 0.5;
+  } else {
+    this.cost += 1;
+  };
+  return this.cost;
+};
 
-      //sizes (4)
-      "Large": 25, "Medium": 19, "Small": 15, "Personal": 10,
 
-      //crusts (3)
-      "Thin": 0.5, "Stuffed": 1, "Original": 0,
+Pizza.prototype.priceForToppings = function() {
+  for (var i = 0; i < this.toppings.length; i++) {
+    this.cost += 0.20;
+  };
+  return this.cost;
+};
 
-      //toppings (6)
-      "Pepperoni": 0.2, "Black Olives": 0.1, "Pineapples": 0.1, "Green Pepper": 0.1, "Mushrooms": 0.1, "Breakfast Bacon": 0.3, "Sausage": 0.2, "Diced Tomatoes": 0.1,
+Pizza.prototype.priceForSauce = function() {
+  if (this.sauce === 'tomato') {
+    this.cost += 0;
+  } else if ( this.sauce === 'alfredo') {
+      this.cost += 0.5;
+  } else if (this.sauce === 'marinara') {
+      this.cost += 0.3;
+  };
+  return this.cost;
+};
 
-      //sause (3)
-      "Alfredo Sauce": 0.5, "Robust Inspired Tomato Sauce": 0, "Hearty Marinara Sauce": 0.3,
-    };
-      return items[item];
 
     //User Innterface Logic
 $(document).ready(function() {
   $("form#pizza_options").submit(function(event) {
     event.preventDefault();
 
-var pizza;
 
-  var size, crust, toppings, sauce = [ ];
+  var size = $("input:radio[name=pizza-size]:checked").val();
+  var crust = $("input:radio[name=crust]:checked").val();
+  var sauce = $("input:radio[name=sauce]:checked").val();
+  var toppings = [];
 
-    size = $("#new-size").val();
-
-    $("input:checkbox[name=topping-choice]:checked").each(function() {
-
+    $("input:checkbox[name=toppings]:checked").each(function() {
+      console.log($(this).val())
       toppings.push($(this).val());
-
     });
 
 
 
-    pizza = new Pizza(size, crust, toppings, sauce);
-
-    $("#button").onclick((total).append("#total_price"));
-
-//    $().text(pizza.price().toFixed());
-
-
-
-  //  pizza.toppings.forEach(function(element) {
-
-    //  $("").append();
-
-    });
-
-    $("#total-price").text("$" + pizza.price().toFixed());
-
+    var newPizza = new Pizza(size, crust, toppings, sauce);
+    var sizePrice = newPizza.priceForSize();
+    var crustPrice = newPizza.priceForCrust();
+    var toppingsPrice = newPizza.priceForToppings();
+    var saucePrice = newPizza.priceForSauce();
+   
+    $("#total_price").text("$" + newPizza.cost.toFixed(2));
   });
-
-};
-};
+});
